f3017fa56e2af1a1aaac9355fc5b6c704a5e0506
lei.wu@jax.org <lei.wu@jax.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-04-24 16:14:10 +0000

    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/maanova@17652 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/Rmaanova.version.R
+++ R/Rmaanova.version.R
@@ -14,7 +14,7 @@
 ######################################################################
 
 Rmaanova.version <- function() {
-  cat("\nCurrent R/maanova version is 0.98.9\n\n")
+  cat("\nCurrent R/maanova version is 1.2.0\n\n")
 }
 
     
