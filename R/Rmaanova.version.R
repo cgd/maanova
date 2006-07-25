c33a1020fe9921405bb5ad79c43b7f6d92615124
lei.wu@jax.org <lei.wu@jax.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-07-25 17:48:34 +0000

    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/maanova@18949 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/Rmaanova.version.R
+++ R/Rmaanova.version.R
@@ -2,8 +2,8 @@
 #
 # Rmaanova.version.R
 #
-# copyright (c) 2001-2004, Hao Wu and Gary A. Churchill, The Jackson Lab.
-# Written Apr, 2004
+# copyright (c) 2001-2004, Lei Wu and Gary A. Churchill, The Jackson Lab.
+# Written July, 2006
 #
 # Licensed under the GNU General Public License version 2 (June, 1991)
 #
@@ -14,7 +14,10 @@
 ######################################################################
 
 Rmaanova.version <- function() {
-  cat("\nCurrent R/maanova version is 1.2.1\n\n")
+    cat("\nCurrent R/maanova version is ")
+    u <- strsplit(library(help = maanova)[[3]][[1]][2], " ")[[1]]
+    cat(u[length(u)])
+    cat("\n\n")
 }
 
     
