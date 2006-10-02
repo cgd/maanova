03d50e32772191b846f5e509937d5f0e4dd8e398
lei.wu@jax.org <lei.wu@jax.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-10-02 20:47:59 +0000

    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/maanova@20354 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ src/util.h
@@ -0,0 +1,10 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <R.h>
+#include <Rmath.h>
+#include <R_ext/PrtUtil.h>
+#include <R_ext/Applic.h>
+#include <R_ext/Random.h>
+
+double median(double *x,  int n);
